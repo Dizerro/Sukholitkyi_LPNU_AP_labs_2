d7d70ed70e8ee336d3b0eb80c8308fc9a6b60f1e
Dizerro <zakzaksukhar@gmail.com>
2025-04-23 22:45:49 +0300

    Feature/lab_5_done

--- lab_5_lvl_2.py
+++ lab_5_lvl_2.py
@@ -11,11 +11,12 @@ def is_root_vertex(graph, start, n):
     dfs(graph, start, visited)
     return all(visited)
 
-def find_root(graph, n):
+def find_all_roots(graph, n):
+    roots = []
     for i in range(n):
         if is_root_vertex(graph, i, n):
-            return i
-    return -1
+            roots.append(i)
+    return roots if roots else [-1]
 
 def read_graph_from_file(filename):
     with open(filename, "r") as f:
@@ -26,6 +27,10 @@ def read_graph_from_file(filename):
             graph[u].append(v)
     return graph, n
 
-def write_output(filename, result):
+def write_output(filename, roots):
     with open(filename, "w") as f:
-        f.write(str(result))
+        f.write(" ".join(map(str, roots)))
+
+graph, n = read_graph_from_file("input.txt")
+roots = find_all_roots(graph, n)
+write_output("output.txt", roots)
